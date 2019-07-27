55056b9bcf07199f72b694f39dbe4ce836692fea
emilian0prime <39347952+emilian0prime@users.noreply.github.com>
2019-07-27 09:16:21 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,82 @@
+const contractSource = '
+contract Blockstore =
+
+  record purchase =
+    { creatorAddress : address,
+      amount      : int }
+
+  record state =
+    { purchases      : map(int, purchase),
+      purchasesLength : int }
+
+  entrypoint init() =
+    { purchases = {},
+      purchasesLength = 0 }
+
+  entrypoint getPay(index : int) : purchase =
+  	switch(Map.lookup(index, state.purchases))
+	    None    => abort("There was no purchases with this index registered.")
+	    Some(x) => x
+
+  stateful entrypoint registerPurchase(url' : string, name' : string) =
+    let purchase = { creatorAddress = Call.caller, url = url', name = name', amount = 0}
+    let index = getPurchasesLength() + 1
+    put(state{ purchases[index] = purchase, purchasesLength = index })
+
+  entrypoint getPurchasesLength() : int =
+    state.purchasesLength
+
+  stateful entrypoint buyProduct(index : int) =
+    let purchase = getPay(index)
+    Chain.spend(purchase.creatorAddress, Call.value)
+    let updatedamount = purchase.amount + Call.value
+    let updatedPurchases = state.purchases{ [index].amount = updatedamount }
+    put(state{ purchases = updatedPurchases })';
+
+
+const contractAddress ='ct_2rAoyeGTzhzJ1dX3wEAKi46L6frH6E7pucrUMRCK9mk5a4yb4v';
+var client = null
+var paymentArray = [];
+var purchasesLength = 0
+
+function renderPayment() {
+    var template = $('#template').html();
+    Mustache.parse(template);
+    var rendered = Mustache.render(template, {});
+    $('#paymentBody').html(rendered);
+}
+
+async function callStatic(func, args) {
+const contract = await client.getContractInstance(contractSource, {contractAddress});
+const calledGet = await contract.call(func, args, {callStatic: true}).catch(e => console.error(e));
+const decodedGet = await calledGet.decode().catch(e => console.error(e));
+
+return decodedGet;
+}
+window.addEventListener('load', async () => {
+    $("#loader").show();
+
+    client = await Ae.Aepp();
+
+    purchasesLength = await callStatic('getPurchasesLength', [])
+
+    for (let i = 1; i <= purchasesLength; i++){
+     const purchase = await callStatic('getPurchase', [i])
+
+     paymentArray.push({
+    index: i,
+    purchases: purchase.amount
+ })
+}
+
+
+    renderPayment();
+});
+
+jQuery("#paymentBody").on("click", ".payBtn", async function(event){
+    const value = $(this).siblings('input').val();
+    const dataIndex = event.target.id;
+    const foundIndex = paymentArray.findIndex(purchase => purchase.index == dataIndex);
+    paymentArray[foundIndex].payments += parseInt(value, 10);
+    renderPayments();
+  });
